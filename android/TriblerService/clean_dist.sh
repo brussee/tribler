882a6d4f79f030fdf406600142b284a42c0398d3
Paul Brussee <brussee@live.nl>
2016-05-20 21:26:07 +0200

    fix build.sh

--- android/TriblerService/clean_dist.sh
+++ android/TriblerService/clean_dist.sh
@@ -2,7 +2,9 @@
 
 echo Clean dist
 
+rm -rf dist/TriblerService/*
+rm -rf /home/paul/.local/share/python-for-android/build/bootstrap_builds/*
+
 p4a clean_dists
 p4a clean_bootstraps
 
-rm -rf dist/TriblerService/*
