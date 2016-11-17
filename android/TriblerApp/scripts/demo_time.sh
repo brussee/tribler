2599f211e89377140a2350c15a3759e5e4dbc35c
Paul Brussee <brussee@live.nl>
2016-11-17 16:43:48 +0100

    grep demo data

--- android/TriblerApp/scripts/demo_time.sh
+++ android/TriblerApp/scripts/demo_time.sh
@@ -1,76 +1,12 @@
 #!/bin/bash
 
-#$ADB logcat -c
-
-#timestamp=$(date +%s)
-
-echo 1
-. ./phone1.sh
-$ADB shell date
-date
-
-echo 2
-. ./phone2.sh
-$ADB shell date
-date
-
-echo 3
-. ./phone3.sh
-$ADB shell date
-date
-
-echo 4
-. ./phone4.sh
-$ADB shell date
-date
-
-echo 5
-. ./phone5.sh
-$ADB shell date
-date
-
-echo 6
-. ./phone6.sh
-$ADB shell date
-date
-
-echo 7
-. ./phone7.sh
-$ADB shell date
-date
-
-echo 8
-. ./phone8.sh
-$ADB shell date
-date
-
-echo 9
-. ./phone9.sh
-$ADB shell date
-date
-
-echo 10
-. ./phone10.sh
-$ADB shell date
-date
-
-echo 11
-. ./phone11.sh
-$ADB shell date
-date
-
-echo 12
-. ./phone12.sh
-$ADB shell date
-date
-
-echo 13
-. ./phone13.sh
-$ADB shell date
-date
-
-echo 14
-. ./phone14.sh
-$ADB shell date
-date
-
+for VAR_i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+do
+    echo $VAR_i
+    . ./phone$VAR_i.sh
+    $ADB shell date
+    date
+    #$ADB root
+    #$ADB logcat -c
+    $ADB logcat -d -v time tag long | grep "V/AddedToChannel" >> phone$VAR_i.dat
+done
